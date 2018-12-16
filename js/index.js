9f9bbeb57907e6093950c62c28e52fd75ec05af1
Yana Byldina <yanabyldina@gmail.com>
2018-12-16 17:25:21 +0000

    Project Created

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ const a = parseFloat(prompt('a')) ;
 const b = parseFloat(prompt('b')) ;
 const c = parseFloat(prompt('c')) ;
 function solveQuadrEq (a, b, c) {
-    const D = calcDiscr (a, b, c,) ;
+    const D = b*b - 4*a*c ;
     
     if (D>0) {
         x1=(-b+Math.sqrt(D))/(2*a);
@@ -11,14 +11,12 @@ function solveQuadrEq (a, b, c) {
         return   'This equation has 2 solutions :  x1= ' + x1 + ', x2= ' + x2;
     } else if (D==0) {
         x1=-b/(2*a) ;
-        return 'This equation has one solution : x12= ' + x; s
+        return 'This equation has one solution : x12= ' + x; 
     } else if (D<0) 
       return 'This equation has no real solutions'  
     }
-     alert ('solveQuadrEq(a,b,c,)) ;
+     alert (solveQuadrEq(a,b,c,)) ;
     
-    //function calcDiscr (a, b,c) {
-     //   let result = quad(a,b,c);
-    alert ('solveQuadrEq(a,b,c,) ;
+    
 
 
