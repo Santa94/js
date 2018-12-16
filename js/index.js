cac04410c64220e700aa1657b7342f82b1b76e15
Yana Byldina <yanabyldina@gmail.com>
2018-12-16 17:11:08 +0000

    Project Created

---
+++ js/index.js
@@ -0,0 +1,24 @@
+
+const a = parseFloat(prompt('a')) ;
+const b = parseFloat(prompt('b')) ;
+const c = parseFloat(prompt('c')) ;
+function solveQuadrEq (a, b, c) {
+    const D = calcDiscr (a, b, c,) ;
+    
+    if (D>0) {
+        x1=(-b+Math.sqrt(D))/(2*a);
+        x2=(-b-Math.sqrt(D))/(2*a);
+        return   'This equation has 2 solutions :  x1= ' + x1 + ', x2= ' + x2;
+    } else if (D==0) {
+        x1=-b/(2*a) ;
+        return 'This equation has one solution : x12= ' + x; s
+    } else if (D<0) 
+      return 'This equation has no real solutions'  
+    }
+     alert ('solveQuadrEq(a,b,c,)) ;
+    
+    //function calcDiscr (a, b,c) {
+     //   let result = quad(a,b,c);
+    alert ('solveQuadrEq(a,b,c,) ;
+
+
